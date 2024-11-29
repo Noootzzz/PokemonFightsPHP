41afceb04bb5629bef8da96852c080825d6125e8
Nathan Bo <nathan.github22@gmail.com>
2024-11-29 08:49:41 +0100

    changement noms pokemons

--- pokemons.php
+++ pokemons.php
@@ -2,8 +2,8 @@
 
 
 $pokemons = [
-    $feuman = new PokemonFeu('FeuMan', 'Feu', 100, 20, 10),
-    $eauman = new PokemonEau('EauMan', 'Eau', 100, 15, 12),
-    $plantman = new PokemonPlante('PlanteMan', 'Plante', 100, 14, 13),
-    $electriqueMan = new PokemonElectrique('ElectriqueMan', 'Electrique', 100, 25, 5),
+    $salamèche = new PokemonFeu('Salamèche', 'Feu', 100, 20, 10),
+    $carapuce = new PokemonEau('Carapuce', 'Eau', 100, 15, 12),
+    $bulbizarre = new PokemonPlante('Bulbizarre', 'Plante', 100, 14, 13),
+    $pikachu = new PokemonElectrique('Pikachu', 'Electrique', 100, 25, 5),
 ];
